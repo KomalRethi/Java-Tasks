69dcb21f4202a214ab6506fc2d52299bcdc44334
KomalRethi <rethi.komal@encora.com>
2025-10-22 15:01:21 +0530

    Delete Day-15-10-2025 directory

--- Day-15-10-2025/SBwithH2db/src/main/java/com/test/repository/EmployeeRespository.java
+++
@@ -1,9 +0,0 @@
-package com.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.test.model.Employee;
-
-public interface EmployeeRespository extends JpaRepository<Employee, Integer>{
-	
-}
